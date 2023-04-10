8ce1ac03524b6b6363cf2e6769e086e082ade22c
Roman Bocharov <roman.boch17@gmail.com>
2023-04-10 13:03:59 +0300

    password

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,29 +8,26 @@ const Login = () => {
   const [log, setLog] = useState("")
   const [pass, setPass] = useState("")
 
-  const login = event => {
-    event.preventDefault();
+const login = (e) => {
+  e.preventDefault();
+  if (log === "1111" && pass === "1111") {
     setIsAuth(true)
-
-  }
+  } {}
+}
 
   return (
 
 
-    <div className='login'>
+    <div className='login' onSubmit={login}>
 
-      <form className='login__content' onSubmit={login}>
+      <form className='login__content'>
         <h3 style={{ padding: '10px' }}>Авторизация</h3>
-        <MyInput onChange={(event) => setLog(event.target.value)} type="text" placeholder='Введите логин' />
-        <MyInput onChange={(event) => setPass(event.target.value)} type="password" placeholder='Введите пароль' />
+        <MyInput onChange={(event) => setLog(event.target.value)} type="text" placeholder='Введите логин 1111' />
+        <MyInput onChange={(event) => setPass(event.target.value)} type="password" placeholder='Введите пароль 1111' />
         <MyButton>Войти</MyButton>
       </form>
 
     </div>
-
-
-
-
   )
 }
 
